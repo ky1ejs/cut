62827568a86b4ad553bce363941b16e7aeb3d779
Kyle Satti <k@kylejs.dev>
2024-02-02 10:05:54 -0500

    CSS tweaks

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
     <main>
-      <div className="flex h-screen w-full">
-        <div className="m-auto text-[80pt]">🎬</div>
+      <div className="background absolute -z-10 top-0 bottom-0 right-0 left-0"/>
+      <div className="flex sm:absolute sm:top-0 sm:bottom-0 sm:right-0 sm:left-0 my-40">
+        <div className="m-auto text-[80pt] h-max">🎬</div>
       </div>
       <div className="fixed bottom-24 w-full">
-        <div className="flex justify-center gap-4">
+        <div className=" flex justify-center gap-4">
           <a href="https://www.threads.net/@fbnsz">@fbnsz</a>
           <a href="https://www.threads.net/@_kylejs_">@kylejs</a>
         </div>
