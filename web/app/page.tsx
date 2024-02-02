12975bd5adee245189f68a557ce735c6f79bc9a3
Kyle Satti <k@kylejs.dev>
2024-02-02 16:54:49 -0500

    Fix CSS again 😅

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div className="absolute bottom-0 left-0 right-0 top-0 -z-10" />
-      <div className="my-40 flex sm:absolute sm:bottom-0 sm:left-0 sm:right-0 sm:top-0">
-        <div className="m-auto h-max text-[80pt]">🎬</div>
+      <div className="background fixed bottom-0 left-0 right-0 top-0" />
+      <div className="fixed bottom-0 left-0 right-0 top-0 flex">
+        <div className="mx-auto mt-36 text-[80pt] sm:my-auto">🎬</div>
       </div>
       <div className="fixed bottom-24 w-full">
         <div className=" flex justify-center gap-4">
