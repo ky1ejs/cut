a3cf5d4c681676deeff6948f210afb4da9aa2993
Kyle Satti <k@kylejs.dev>
2024-02-02 10:12:31 -0500

    LInter

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div className="background absolute -z-10 top-0 bottom-0 right-0 left-0"/>
-      <div className="flex sm:absolute sm:top-0 sm:bottom-0 sm:right-0 sm:left-0 my-40">
-        <div className="m-auto text-[80pt] h-max">🎬</div>
+      <div className="background absolute bottom-0 left-0 right-0 top-0 -z-10" />
+      <div className="my-40 flex sm:absolute sm:bottom-0 sm:left-0 sm:right-0 sm:top-0">
+        <div className="m-auto h-max text-[80pt]">🎬</div>
       </div>
       <div className="fixed bottom-24 w-full">
         <div className=" flex justify-center gap-4">
