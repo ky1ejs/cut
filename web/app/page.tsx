fe9b2bf6f845412e982999eca6a521acff703645
Kyle Satti <k@kylejs.dev>
2024-02-02 16:57:10 -0500

    move handles close to bottom

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="fixed bottom-0 left-0 right-0 top-0 flex">
         <div className="mx-auto mt-36 text-[80pt] sm:my-auto">🎬</div>
       </div>
-      <div className="fixed bottom-24 w-full">
+      <div className="fixed bottom-8 w-full">
         <div className=" flex justify-center gap-4">
           <a href="https://www.threads.net/@fbnsz">@fbnsz</a>
           <a href="https://www.threads.net/@_kylejs_">@kylejs</a>
