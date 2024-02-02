0d7df46d4d1cd180df3a92b9a651c0f4d98a7db3
Kyle Satti <k@kylejs.dev>
2024-02-01 20:57:38 -0500

    Adding NextJS site

---
+++ web/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="flex h-screen w-full">
+        <div className="m-auto text-[80pt]">🎬</div>
+      </div>
+      <div className="fixed bottom-24 w-full">
+        <div className="flex justify-center gap-4">
+          <a href="https://www.threads.net/@fbnsz">@fbnsz</a>
+          <a href="https://www.threads.net/@_kylejs_">@kylejs</a>
+        </div>
+      </div>
+    </main>
+  );
+}
