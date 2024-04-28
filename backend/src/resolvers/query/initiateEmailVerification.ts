a86eccd4f918e1505757adc0b5ad89042c567918
Kyle Satti <k@kylejs.dev>
2024-04-27 20:55:40 -0400

    Fix email regex

--- backend/src/resolvers/query/initiateEmailVerification.ts
+++ backend/src/resolvers/query/initiateEmailVerification.ts
@@ -14,7 +14,7 @@ const initiateEmailVerification: QueryResolvers["initiateEmailVerification"] = a
   }
 
   // validate email
-  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/
+  const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
   if (!emailRegex.test(args.email)) {
     throw new Error("Invalid email")
   }
