d9392c6958900e595897aca704ccc1f4f636eb40
Kyle Satti <k@kylejs.dev>
2024-05-15 21:21:59 -0400

    Fix sheet presentation from watch list

--- ios/Cut/WatchList.swift
+++ ios/Cut/WatchList.swift
@@ -11,18 +11,24 @@ import Apollo
 struct WatchList: View {
     @State var movies: [Movie] = []
     @State private var watcher: GraphQLQueryWatcher<CutGraphQL.WatchListQuery>?
+    @State private var presentedContent: Movie?
 
     var body: some View {
         NavigationStack {
             List {
                 ForEach(movies, id: \.id) { m in
-                    NavigationLink(destination: DetailView(content: m)) {
+                    Button(action: {
+                        presentedContent = m
+                    }, label: {
                         ContentRow(movie: m)
-                    }
+                    })
                 }
             }
             .listStyle(.plain)
         }
+        .sheet(item: $presentedContent, content: { c in
+            DetailView(content: c)
+        })
         .task {
             self.watcher?.cancel()
             self.watcher = AuthorizedApolloClient.shared.client.watch(query: CutGraphQL.WatchListQuery(), resultHandler: { result in
