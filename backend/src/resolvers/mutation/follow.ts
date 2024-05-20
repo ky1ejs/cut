ba10a2d60716d389fd086f1a57976449d954aedc
Kyle Satti <k@kylejs.dev>
2024-05-19 20:21:32 -0400

    Fix push limit checking

--- backend/src/resolvers/mutation/follow.ts
+++ backend/src/resolvers/mutation/follow.ts
@@ -38,11 +38,7 @@ const follow: MutationResolvers["follow"] = async (_, args, context) => {
     }
   })
 
-  if (latestPush?.repeatLimit) {
-    console.log("Latest push", latestPush)
-    console.log("Time since last push", Date.now() - latestPush.createdAt.getTime())
-  }
-  if (!latestPush?.repeatLimit || latestPush.repeatLimit > (Date.now() - latestPush.createdAt.getTime())) {
+  if (!latestPush?.repeatLimit || latestPush.repeatLimit <= (Date.now() - latestPush.createdAt.getTime())) {
     const devices = await prisma.device.findMany({
       where: {
         userId: args.userId
